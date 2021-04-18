3ec60bd04145182e0aedd4d5580b59e5919e8f4f
LetsPoke <59581396+LetsPoke@users.noreply.github.com>
2021-04-18 18:50:08 +0200

    First Push

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    //public Transform target;
+    private Vector3 offset;
+
+    private void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+
+        transform.position = player.transform.position + ((Quaternion.Slerp(player.transform.rotation, transform.rotation,2))*offset);
+
+        transform.rotation = player.transform.rotation * Quaternion.Euler(20, 0, 0);
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    
+    [SerializeField] private float movementSpeed = 0f;
+
+    private float angle;
+    public int score = 0;
+    [SerializeField] public Text scoreText;
+    
+    
+    void Start(){
+        transform.position = new Vector3(0f,1f,0f);
+        scoreText.text = "Score: 0";
+    }
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        angle += moveHorizontal*0.02f;
+        Vector3 targetDirection = new Vector3(Mathf.Sin(angle), 0f, Mathf.Cos(angle));
+
+        transform.rotation = Quaternion.LookRotation(targetDirection);
+        
+        transform.position += moveVertical * movementSpeed * transform.forward;
+
+
+        if (Input.GetKeyDown("space")){
+            this.transform.rotation = Quaternion.Euler(new Vector3(0f,0f,0f));
+        }
+        
+        scoreText.text = "Score: " + score;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.gameObject.tag.Equals("coin")){
+        score++;
+        Debug.Log(score);
+        }
+        
+    }
+
+}
