c8cb17385e3dfcae997137f525cd729ba3683d92
LetsPoke <59581396+LetsPoke@users.noreply.github.com>
2021-04-23 11:37:13 +0200

    added character animation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,16 +7,25 @@ public class CameraController : MonoBehaviour
     [SerializeField] private GameObject player;
     //public Transform target;
     private Vector3 offset;
+    public float zoomSpeed = 4f;
+    public float minZoom = 1f;
+    public float maxZoom = 3f;
+    private float currentZoom = 1f;
 
     private void Start()
     {
         offset = transform.position - player.transform.position;
     }
 
+     void Update() {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+    }
+
     private void LateUpdate()
     {
 
-        transform.position = player.transform.position + ((Quaternion.Slerp(player.transform.rotation, transform.rotation,2))*offset);
+        transform.position = player.transform.position + ((Quaternion.Slerp(player.transform.rotation, transform.rotation,2))*offset) * currentZoom/10; //* currentZoom
 
         transform.rotation = player.transform.rotation * Quaternion.Euler(20, 0, 0);
         
